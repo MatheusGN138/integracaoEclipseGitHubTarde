bdbc228323b2b5a5b1d14eef635f7487ade0c220
matheus62243676 <matheus62243676@BUE0108D012.alunos.sc.senac.br>
2026-06-17 15:58:54 -0300

    Teste

---
+++ projetogithub/src/projetogithub/ProjetoGitHubTarde.java
@@ -0,0 +1,10 @@
+package projetogithub;
+
+public class ProjetoGitHubTarde {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("primeira classe github");
+	}
+
+}
