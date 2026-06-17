2f13191a8e033df095025c9008c6f101254ca09a
MatheusGN138 <m.g.nicolletti@gmail.com>
2026-06-17 16:05:49 -0300

    Update ProjetoGitHubTarde.java

--- projetogithub/src/projetogithub/ProjetoGitHubTarde.java
+++ projetogithub/src/projetogithub/ProjetoGitHubTarde.java
@@ -3,8 +3,8 @@ package projetogithub;
 public class ProjetoGitHubTarde {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("primeira classe github");
+		System.out.println("segundo commit");
 	}
 
 }
